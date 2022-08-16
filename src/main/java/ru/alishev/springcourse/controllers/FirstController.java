148763b46e32f4421273e9b21dc48aaf5b6ee3f0
abahaan.kokov@gmail.com <pOj65xT69>
2022-08-17 01:55:49 +0700

    Param from get

--- src/main/java/ru/alishev/springcourse/controllers/FirstController.java
+++ src/main/java/ru/alishev/springcourse/controllers/FirstController.java
@@ -3,6 +3,9 @@ package ru.alishev.springcourse.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 //аннотация RequestMapping("/url") над классом говорит, что все методы будут вызываться  с этим префиксом урл
@@ -10,7 +13,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class FirstController {
     //связывание урл с методом, который возвращает название представления
     @GetMapping("/hello")
-    public String helloPage(){
+    public String helloPage(@RequestParam(value = "name", required = false) String name,//аннотация внедряет параметры из гет запроса в переменные.
+                            @RequestParam(value = "surname", required = false) String surname){//required = false передает нал если нет параметров
+//        String name = request.getParameter("name");
+//        String surname = request.getParameter("surname");
+
+        System.out.println("Hello, " + name + "  " + surname);
+
         return "first/hello";
     }
     @GetMapping("/goodbye")
